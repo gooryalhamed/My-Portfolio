c74785996648c9810bb5d582136c58af3b67db5e
Goory al Hamed <gooryalhamed@hotmail.com>
2017-07-27 16:47:47 +0300

    add mail_form gem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,2 @@
 class UserMailer < ApplicationMailer
-	def send_to_me(msg)
-		mail(to: "gooryalhamed@gmail.com")
-	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings= {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "safe-garden-26180.herokuapp.com",
+    domain: "localhost",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
