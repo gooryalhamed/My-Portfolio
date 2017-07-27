830862ed6e17238524ae543e33ef5958dd063711
Goory al Hamed <gooryalhamed@hotmail.com>
2017-07-27 23:27:01 +0300

    successfully recieving emails in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,14 +52,12 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings= {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "localhost",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
-
-end
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            'gooryalhamed@gmail.com',
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  end
