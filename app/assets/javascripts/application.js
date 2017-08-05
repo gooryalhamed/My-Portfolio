c017e67c69566b99a7cf0f1b0c76c754d68d0154
Goory al Hamed <gooryalhamed@hotmail.com>
2017-08-05 19:36:58 +0300

    adjust navbar links margin on mobile devise and correct some text on home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+$(document).ready(function(){
+	$(".custom-header .nav li").click(function(){
+	$(this).addClass("selected");
+});
+})
