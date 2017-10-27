c3e7dd495cfc10a112b2de5cb20faa91678fa0a9
gooryalhamed <gooryalhamed@hotmail.com>
2017-10-27 12:32:59 +0300

    remove turbolinks and highlight the current page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,26 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require bootstrap-sprockets
 $(document).ready(function(){
-	$(".custom-header .nav li").click(function(){
-	
-});
+	checkPath();
 })
+var current_page = window.location.pathname;
+function checkPath(){
+	if (current_page == "/") {
+		$('.custom-header a').removeClass('selected');
+		$('#home').addClass('selected');
+	}
+	else if (current_page == "/my-cv") {
+		$('.custom-header a').removeClass('selected');
+		$('#cv').addClass('selected');
+	}
+	else if (current_page == "/my-projects") {
+		$('.custom-header a').removeClass('selected');
+		$('#projects').addClass('selected');
+	}
+	else if (current_page == "/contacts/new") {
+		$('.custom-header a').removeClass('selected');
+		$('#contact').addClass('selected');
+	}
+}
